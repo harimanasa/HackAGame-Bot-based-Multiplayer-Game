f98d638388f3b12b31d6f2d2a383e21d7040f17c
Manasa Hari <harimanasa17@gmail.com>
2019-11-17 01:43:22 -0800

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,20 @@
+var questionsMap = [];
+questionsMap.push('Manasa', 'Hari');
+
+console.log(questionsMap[0]);
+
+
+// if(count==2){
+//     console.log('Game over ');
+//     console.log(message);
+//     bot.sendTextMessage(userArray[1], msgmap.get(userArray[0]));
+//     bot.sendTextMessage(userArray[0], msgmap.get(userArray[1]));
+//     //bot.sendTextMessage(userArray[0], msgmap.get(userArray[0]));
+// }
+// if(userCount==2){
+//     if(start ==0) {
+//         start = 1
+//         bot.sendTextMessage(userId, 'We got enough users! Stop making faces. ');
+//     }
+//     questionsCount = 0, answersCount = 0;
+// }
